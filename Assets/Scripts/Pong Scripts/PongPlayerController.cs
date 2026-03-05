a0a14d81d73a5f2dc8d2e3ca020d9736f0fea142
glaboy02 <glaboy02@gmail.com>
2026-03-05 12:28:52 -0500

    Fixed pong player2 movement not working in Web Build

--- Assets/Scripts/Pong Scripts/PongPlayerController.cs
+++ Assets/Scripts/Pong Scripts/PongPlayerController.cs
@@ -6,29 +6,55 @@ using UnityEngine.InputSystem;
 public class PongPlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private InputActionAsset inputActions;
+    [SerializeField] private string actionMapName;  // "Player1" or "Player2"
+    [SerializeField] private string actionName;     // "Move1" or "Move2"
+
     private Rigidbody2D rb;
-    private float moveInput;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private InputAction moveAction;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        var map = inputActions.FindActionMap(actionMapName, throwIfNotFound: true);
+        moveAction = map.FindAction(actionName, throwIfNotFound: true);
+        moveAction.Enable();
     }
 
-    public void Move(InputAction.CallbackContext context)
+    void Update()
     {
         if (GameManager.GameplayPaused) return;
 
-        moveInput = context.ReadValue<Vector2>().y;
+        float moveInput = moveAction.ReadValue<Vector2>().y;
         rb.linearVelocity = new Vector2(0f, moveInput * moveSpeed);
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    void OnDestroy()
     {
-        // if (other.gameObject.CompareTag("Ball"))
-        // {
-        //     // Add logic for ball collision, e.g., play sound, increase score, etc.
-
-        // }
+        moveAction?.Disable();
     }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // void Start()
+    // {
+    //     rb = GetComponent<Rigidbody2D>();
+    // }
+
+    // public void Move(InputAction.CallbackContext context)
+    // {
+    //     if (GameManager.GameplayPaused) return;
+
+    //     moveInput = context.ReadValue<Vector2>().y;
+    //     rb.linearVelocity = new Vector2(0f, moveInput * moveSpeed);
+    // }
+
+    // private void OnCollisionEnter2D(Collision2D other)
+    // {
+    //     // if (other.gameObject.CompareTag("Ball"))
+    //     // {
+    //     //     // Add logic for ball collision, e.g., play sound, increase score, etc.
+
+    //     // }
+    // }
 
 }
